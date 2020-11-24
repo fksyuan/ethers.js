64de0f3d3f901f25aecf44103e81b7c3c214f124
yuanwenjun <yuanwenjun@juzix.net>
2020-11-24 13:25:15 +0800

    update address

--- packages/abi/lib.esm/coders/address.js
+++ packages/abi/lib.esm/coders/address.js
@@ -2,6 +2,7 @@
 import { getAddress } from "@ethersproject/address";
 import { hexZeroPad } from "@ethersproject/bytes";
 import { Coder } from "./abstract-coder";
+import {isBech32Address, decodeBech32Address} from '@alayanetwork/ethereumjs-util'
 export class AddressCoder extends Coder {
     constructor(localName) {
         super("address", "address", localName, false);
@@ -12,6 +13,9 @@ export class AddressCoder extends Coder {
         }
         catch (error) {
             this._throwError(error.message, value);
+        }
+        if (isBech32Address(value)) {
+            value = decodeBech32Address(value);
         }
         return writer.writeValue(value);
     }
